b1ff8f5d6bb88fd94fec6b1a4918f23af15978ff
diaz1202 <alejodiaz.1202@gmail.com>
2014-08-29 08:15:15 -0600

    Desarrolladores

---
+++ HDT6/src/Desarrolladores.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alejo
+ */
+public class Desarrolladores {
+    private int grupo;
+    private String nombre;
+    
+    Desarrolladores(String nombre, int grupo)
+    {
+        this.nombre=nombre;
+        this.grupo=grupo;
+    }
+    
+}
